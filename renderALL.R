02f8059407d433b8066be99a0fab40278aa40b38
Logan Kelly <logan.kelly@uwrf.edu>
2021-01-10 14:35:58 -0600

    R studio commit

--- renderALL.R
+++ renderALL.R
@@ -1,12 +1,10 @@
 # Compile all tutorial pages
 library(markdown)
 library(rmarkdown)
-out.dir <- paste(getwd(),"docs",sep="/")
-#rmarkdown::render("rmd/4-1_prediction.Rmd", output_dir = "docs")
 
 
 render_site(
-  input = "RMD",
+  input = ".",
   output_format = "all",
   envir = parent.frame(),
   quiet = FALSE,
